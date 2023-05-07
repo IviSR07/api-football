fd3eca8132457e36928c76bdfe51a59f5573f27d
IviSR07 <ivansalinerodelosreyes@gmail.com>
2023-05-07 11:53:24 +0200

    Fixed matches

--- match.py
+++ match.py
@@ -1,4 +1,4 @@
 class Match
-def __init__ (self,local,visitante)
-self.local=local
-self.visitante=visitante
+    def __init__ (self,local,visitante):
+    self.local=local
+    self.visitante=visitante
